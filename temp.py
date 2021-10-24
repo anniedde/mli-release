f296fe68e2c27f71e84f26bf6b0ce3613fa38e1a
Annie Wang <aw364@cs.duke.edu>
2021-10-24 13:35:29 -0400

    inital commit

--- temp.py
+++ temp.py
@@ -1,1 +1,21 @@
-print('hi!')
+import copy
+import functools
+import os
+import sys
+
+import json
+import numpy as np
+import torch
+import torch.nn.functional as F
+import tqdm
+from sacred import Experiment
+from torch.optim.lr_scheduler import MultiStepLR
+
+from munch import Munch
+import sys
+
+checkpoint = torch.load('/usr/xtmp/CSPlus/VOLDNN/Annie/mli-release/runs/mli_cifar10/1572454/final.pt', map_location=torch.device('cpu'))
+#start_epoch = checkpoint["epoch"]
+#model.load_state_dict(checkpoint["model_state"])
+
+print(checkpoint)
